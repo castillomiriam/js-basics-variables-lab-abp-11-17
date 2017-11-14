24feb03085cceb8ba643d45eff148b5209d583a5
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-14 03:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-let companyName = 'Scuber';
-const companyName = 'specialCompany';
+const companyName = 'Scuber';
+let companyName = 'specialCompany';
+let mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Upper West Side';
+let companyCeo = 'Susan Smith'
+let companyCeo = 'Lauren Hart'
